387539351b09183c670354f086dcecdedd2abbec
Alif Jamaluddin <alifjamaluddin92@gmail.com>
2015-08-19 12:28:06 +0800

    Add devise user model and post scaffold

---
+++ db/migrate/20150819042333_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :content
+      t.string :cover
+      t.references :user, index: true
+
+      t.timestamps null: false
+    end
+    add_foreign_key :posts, :users
+  end
+end
